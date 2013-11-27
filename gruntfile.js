6721ade0c2efdd70e42b0c2f2a22b8282dfe6906
Avenger7x <avenger7x13@gmail.com>
2013-11-27 10:41:56 +0200

    initial commit

---
+++ gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		bump: {
+			options: {
+				files: ['package.json', 'bower.json'],
+				commitFiles: ['-a'],
+				commitMessage: 'release %VERSION%',
+				tagMessage: 'version %VERSION%',
+				pushTo: 'origin'
+			}
+		}
+	})
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-complexity');
+	grunt.loadNpmTasks('grunt-bump');
+
+	grunt.registerTask('default', ['jshint', 'complexity', 'uglify']);
+};
