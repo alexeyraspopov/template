93188d7b8165af63a452b441665d2e38202a4d03
Avenger7x <avenger7x13@gmail.com>
2013-11-28 00:13:00 +0200

    used imperative version of handlers for now

--- src/handlers.js
+++ src/handlers.js
@@ -1,3 +1,15 @@
-text = function(element, value){
-	element.textContent = value;
+text = function(element, scope, attrs){
+	observe(scope, attrs.text, function(value){
+		element.textContent = value;
+	});
+};
+
+model = function(element, scope, attrs){
+	observe(scope, attrs.model, function(value){
+		element.value = value;
+	});
+
+	element.addEventListener('keyup', function(){
+		scope[attrs.model] = this.value;
+	});
 };
