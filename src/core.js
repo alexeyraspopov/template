960727d0b7f13acf11747fb20cc098f2f8ee408f
Avenger7x <avenger7x13@gmail.com>
2013-11-28 10:18:01 +0200

    define binding objects

--- src/core.js
+++ src/core.js
@@ -1,8 +1,11 @@
-function View(root, model){
+function View(root, scope){
 	this.element = root;
-	this.model = model;
+	this.scope = scope;
+	this.bindings = [];
 
 	this.compile();
+	// IDEA: use fastdom for bind handlers
+	this.bind();
 }
 
 View.prototype.compile = function(){
@@ -12,18 +15,41 @@ View.prototype.compile = function(){
 		node = walker.currentNode;
 
 	do{
-		this.bind(node);
+		this.createBindings(node);
 		node = walker.nextNode(); // or nextSibling()
 	}while(node);
 };
 
-View.prototype.bind = function(node){
+View.prototype.createBindings = function(node){
 	// TODO: create Binding contructor for each case of binding
 	Object.keys(node.dataset).filter(function(key){
 		return !!window[key];
 	}).map(function(key){
-		return window[key];
+		return window[key] instanceof Function ? { routine: window[key] } : window[key];
 	}).forEach(function(handler){
-		handler(node, this.model, node.dataset);
+		// set binding type (dataset key)
+		this.bindings.push(new Binding(node, this.scope, handler));
 	}, this);
+};
+
+View.prototype.bind = function(){
+	this.bindings.forEach(function(binding){
+		binding.bind();
+	});
+};
+
+function Binding(element, scope, handler){
+	this.element = element;
+	this.scope = scope;
+	this.handler = handler;
+
+	// TODO: use routine only with two arguments: element, value
+	// TODO: observe value for binding for calling routine
+	this.handler.routine(element, scope, element.dataset);
+}
+
+Binding.prototype.bind = function(){
+	if(this.handler.bind){
+		this.handler.bind(this.element, this.scope, this.element.dataset);
+	}
 };
