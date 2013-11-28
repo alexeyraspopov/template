2b0e7c284f2f789646aac01dcb31f8c32dbe95d6
Avenger7x <avenger7x13@gmail.com>
2013-11-28 10:17:09 +0200

    change handlers structure. splitted functionality

--- src/handlers.js
+++ src/handlers.js
@@ -4,12 +4,18 @@ text = function(element, scope, attrs){
 	});
 };
 
-model = function(element, scope, attrs){
-	observe(scope, attrs.model, function(value){
-		element.value = value;
-	});
+model = {
+	routine: function(element, scope, attrs){
 
-	element.addEventListener('change', function(){
-		scope[attrs.model] = this.value;
-	});
+		observe(scope, attrs.model, function(value){
+			element.value = value;
+		});
+	},
+	bind: function(element, scope, attrs){
+
+		element.addEventListener('change', function(){
+			// TODO: work with nested keypath
+			scope[attrs.model] = this.value;
+		});
+	}
 };
