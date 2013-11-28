e2928a2fbe5d11cb41b6ed09a6b08129a3fbc888
Avenger7x <avenger7x13@gmail.com>
2013-11-28 10:17:34 +0200

    observe keypath v1

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,24 @@
-// TODO: work with nested key paths
 // TODO: use only one observer per object
 // TODO: unobserve function
 function observe(target, path, callback){
+	var tokens = path.split('.'),
+		name = tokens.pop() || path;
+
+	// TODO: create observer for each token (observers chain)
+	// TODO: check object existence
+	if(tokens.length){
+		target = tokens.reduce(function(target, key){
+			return target[key];
+		}, target);
+	}
+
 	Object.observe(target, function(changes){
 		changes.filter(function(record){
-			return record.name === path;
+			return record.name === name;
 		}).forEach(function(record){
 			callback(record.object[record.name]);
 		});
 	});
+
+	callback(target[name]);
 }
