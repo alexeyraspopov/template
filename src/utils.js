ad7c53819ab266b7475a97ebab43435637b7a082
Avenger7x <avenger7x13@gmail.com>
2013-12-02 19:24:42 +0200

    DRY

--- src/utils.js
+++ src/utils.js
@@ -1,24 +1,37 @@
 // TODO: use only one observer per object
 // TODO: unobserve function
 function observe(target, path, callback){
-	var tokens = path.split('.'),
-		name = tokens.pop() || path;
-
 	// TODO: create observer for each token (observers chain)
 	// TODO: check object existence
-	if(tokens.length){
-		target = tokens.reduce(function(target, key){
-			return target[key];
-		}, target);
-	}
+	target = targetByPath(target, path);
 
-	Object.observe(target, function(changes){
+	Object.observe(target.object, function(changes){
 		changes.filter(function(record){
-			return record.name === name;
+			return record.name === target.key;
 		}).forEach(function(record){
 			callback(record.object[record.name]);
 		});
 	});
 
-	callback(target[name]);
+	callback(target.object[target.key]);
+}
+
+// TODO: reduce duplication
+function write(target, path, value){
+	target = targetByPath(target, path);
+	target.object[target.key] = value;
+}
+
+function targetByPath(target, path){
+	var tokens = path.split('.'),
+		key = tokens.pop() || path;
+
+	target = tokens.reduce(function(target, key){
+		return target[key];
+	}, target);
+
+	return {
+		object: target,
+		key: key
+	};
 }
