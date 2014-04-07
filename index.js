89f5cd764a709c93209c3f5e5b693d5f9071beca
Avenger7x <avenger7x13@gmail.com>
2014-04-07 14:27:16 +0300

    simple one-way binding

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your awesome code here
+/* jshint browser:true */
+'use strict';
+// TODO: one-way binding
+
+function createTreeWalker(root){
+	var filter = { acceptNode: function(){ return NodeFilter.FILTER_ACCEPT; } },
+		walker = document.createTreeWalker(root, NodeFilter.SHOW_ELEMENT, filter, false);
+
+	return walker;
+}
+
+function compile(root, model, bind){
+	var walker = createTreeWalker(root),
+		node = walker.currentNode;
+
+	do{
+		bind(node, model);
+		node = walker.nextNode(); // .nextSibling();
+	}while(node);
+}
+
+function bind(node, model){
+	Object.keys(node.dataset).map(function(key){
+		node[key] = model[node.dataset[key]];
+	});
+}
+
